44f0d60bfb02d9418b21cb2c8b69dc26357c0a43
pkjkumar66 <pankajkumar130697@gmail.com>
2023-02-05 01:53:00 +0530

    updated config

--- src/main/java/com/example/scheduler/config/SpringFoxConfig.java
+++ src/main/java/com/example/scheduler/config/SpringFoxConfig.java
@@ -1,16 +1,16 @@
 package com.example.scheduler.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
-public class SwaggerConfig {
+@Configuration
+public class SpringFoxConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
